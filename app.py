3ec38cd14b68631088a85c117ee3336b157c2b41
Fabiano Cunha <cunhasb@aol.com>
2018-05-04 03:07:57 -0400

    xxxx

--- app.py
+++ app.py
@@ -292,8 +292,7 @@ def newInboundMessage(user_id):
         print (user.id, customer.id,
                request.form['message_uuid'], request.form['text'])
         newMessage = Message(
-            user_id=user.id, user_customer_id=customer.id, message_uuid=request.form[
-                'message_uui'])
+            user_id=user.id, user_customer_id=customer.id, message_uuid=request.form['message_uuid'], message=request.form['text'], direction="outbound")
         print('newMessage', newMessage)
         db_session.add(newMessage)
         db_session.commit()
