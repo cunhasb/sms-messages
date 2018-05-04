8856126094a3a8da94acc0d9fef39480d79d3012
Fabiano Cunha <cunhasb@aol.com>
2018-05-04 03:46:55 -0400

    xxx.

--- app.py
+++ app.py
@@ -289,13 +289,9 @@ def newInboundMessage(user_id):
             units=request.form["units"],
             total_rate=request.form["total_rate"],
             total_amount=request.form["total_amount"], error_code=request.form["error_code"])
-        print (user.id, customer.id,
-               request.form['message_uuid'], request.form['text'], request.form['status'])
-        newMessage = Message(
-            user_id=user.id, user_customer_id=customer.id, message_uuid=request.form['message_uuid'], message=request.form['text'], direction="INBOUND")
-        print('newMessage', newMessage)
-        # db_session.add(newMessage)
-        # db_session.commit()
+
+        db_session.add(newMessage)
+        db_session.commit()
 
     return jsonify(newMessage.serialize)
 
