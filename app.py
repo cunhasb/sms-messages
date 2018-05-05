12b4a3857e453f4ddcafc25375f2203a6b0b79c1
Fabiano Cunha <cunhasb@aol.com>
2018-05-05 16:33:26 -0400

    xxx

--- app.py
+++ app.py
@@ -443,25 +443,26 @@ def newInboundMessage():
     try:
         print ('requestform', request.form)
         tel = str(request.form['To'])
-        print("type(%s) = %s - type of To=%s %s, tel=to== %s" % (tel, type(tel), request.form['To', type(request.form['To'])])
-        first=db_session.query(User).filter_by(phone=tel)
+        print("type(%s) = %s - type of To=%s %s, tel=to== %s" % (tel, type(tel),
+                                                                 request.form['To'], type(request.form['To']), tel == first.phone))
+        first = db_session.query(User).filter_by(phone=tel)
         print('first=%s' % first.first().phone)
-        user=db_session.query(User).filter_by(
+        user = db_session.query(User).filter_by(
             phone=tel).one()
 
         if user:
-            customer=db_session.query(
+            customer = db_session.query(
                 User_Customer).filter_by(phone=request.form['From']).first()
             if customer:
                 if request.form['Text'] == "UNSUBSCRIBE":
-                    customer.status="UNSUBSCRIBED"
+                    customer.status = "UNSUBSCRIBED"
             else:
-                customer=User_Customer(
+                customer = User_Customer(
                     name='UNKNOWN', phone=request.form['From'], user_id=user.id, status="SUBSCRIBED")
                 db_session.add(customer)
                 db_session.commit()
 
-            newMessage=Message(
+            newMessage = Message(
                 user_id=user.id, user_customer_id=customer.id, message_uuid=request.form['MessageUUID'], message=request.form['Text'], direction="inbound", status="RECEIVED",
                 units=request.form["Units"],
                 total_rate=request.form["TotalRate"],
@@ -479,7 +480,7 @@ def newInboundMessage():
 @app.route('/users/JSON/')
 @login_required
 def usersJSON():
-    users=db_session.query(User).all()
+    users = db_session.query(User).all()
     return jsonify(Users=[i.serialize for i in users])
 
 
@@ -487,8 +488,8 @@ def usersJSON():
 @login_required
 def userCustomersJSON(user_id):
     # pdb.set_trace()
-    user=db_session.query(User).filter_by(id=user_id).one()
-    customers=db_session.query(
+    user = db_session.query(User).filter_by(id=user_id).one()
+    customers = db_session.query(
         User_Customer).filter_by(user_id=user_id).all()
     return jsonify(User=[i.serialize for i in user], User_Customer=[i.serialize for i in customers])
 
@@ -496,14 +497,14 @@ def userCustomersJSON(user_id):
 @app.route('/messages/JSON')
 @login_required
 def messagesJSON():
-    messages=db_session.query(Message).all()
+    messages = db_session.query(Message).all()
     return jsonify(Message=[i.serialize for i in messages])
 
 
 @app.route('/user/api-clients/JSON')
 @login_required
 def apiClientsJSON():
-    apiClients=db_session.query(User_Api_Client).all()
+    apiClients = db_session.query(User_Api_Client).all()
     return jsonify(User_Api_Client=[i.serialize for i in apiClients])
 
 
